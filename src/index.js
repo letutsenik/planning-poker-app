b2af273f3ff41a236e809eb1ff650d1ab78f8d42
Dmitri Lipski <dlipski@klika-tech.com>
2020-05-21 10:22:44 +0300

    Do some refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-const path = require('path');
 const http = require('http');
-const express = require('express');
 const socketio = require('socket.io');
+const app = require('./app');
 const Filter = require('bad-words');
 const {
 	generateMessage,
@@ -23,14 +22,10 @@ const {
 const {addVote, getVoteByRoom, clearVotesByRoom} = require('./services/votes');
 const {statsCount} = require('./utils');
 
-const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 5000;
-const publicDirectoryPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
 	console.log('New WebSocket connection');
