f21fbf8732f67cd1220fef8dd415f4436f4b418d
Dmitri Lipski <dlipski@klika-tech.com>
2020-06-01 09:45:45 +0300

    Add user and room models

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	name: { type: String, default: 'unknown' },
+	roomId: Number,
+	vote: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,17 +1,47 @@
 const createUserService = Users => {
-	const addUser = ({ username = 'unknown', roomId }) => {};
-	const removeUser = (userId) => {};
-	const getUser = (userId) => {};
-	const getUsersInRoom = (roomId) => {};
+	const addUser = async ({ username = 'unknown', roomId }) => {
+		const user = new Users({ name: username.trim(), roomId });
+
+		try {
+			await user.save();
+			return { user };
+		} catch (error) {
+			return { error };
+		}
+	};
+	const removeUser = async userId => {
+		try {
+			const user = await Users.findOneAndDelete(userId);
+			return { user };
+		} catch (error) {
+			return { error };
+		}
+	};
+	const getUser = async userId => {
+		try {
+			const user = await Users.getById(userId);
+			return { user };
+		} catch (error) {
+			return { error };
+		}
+	};
+	const getUsersInRoom = async roomId => {
+		try {
+			const users = await Users.find({ roomId });
+			return { users };
+		} catch (error) {
+			return { error };
+		}
+	};
 
 	return {
 		addUser,
 		removeUser,
 		getUser,
-		getUsersInRoom
-	}
+		getUsersInRoom,
+	};
 };
 
 module.exports = {
-	createUserService
+	createUserService,
 };
