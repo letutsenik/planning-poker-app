029dbbf7e46cb289ffed332e7a3736d53c3fa803
Dmitri Lipski <dlipski@klika-tech.com>
2020-07-21 16:10:06 +0300

    TS migration for clearVote controller

---
+++ src/controllers/clearVote.ts
@@ -0,0 +1,29 @@
+import { User } from '../models/user';
+import { createUserService } from '../services/users.service';
+import { Server, Socket } from 'socket.io';
+import { SendVoteControllerOptions } from './sendVote';
+import { ControllerCallBackType } from '../types';
+
+const userService = createUserService(User);
+
+export const createClearVoteController = (io: Server, socket: Socket) => {
+	return async (
+		options: SendVoteControllerOptions,
+		callback: ControllerCallBackType,
+	) => {
+		const { error, user } = await userService.getUser({ socketId: socket.id });
+		if (error) {
+			return callback(error);
+		}
+		const { error: clearVotesError } = await userService.clearVotesByRoom(
+			String(user?.roomId),
+		);
+		if (clearVotesError) {
+			return callback(clearVotesError);
+		}
+		io.to(String(user?.roomId)).emit('voteListUpdate', {
+			voteData: [],
+		});
+		callback();
+	};
+};
