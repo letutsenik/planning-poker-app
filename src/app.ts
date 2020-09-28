fa7f28a64dd3adae4eb07b8496d3fb543a13a71f
Dmitri Lipski <dlipski@klika-tech.com>
2020-09-28 16:12:14 +0300

    Update tsconfig. Update run script

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import roomRouter from './api/routers/room';
 
 const app = express();
 
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, '../../public');
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 app.use(userRouter);
